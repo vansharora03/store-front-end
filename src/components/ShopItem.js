dfb1d7e7aa42e3db19b6bc47b52f65bc9bc2304a
vansharora03 <vansh1993@icloud.com>
2023-01-19 16:51:19 -0500

    Implemented ShopView

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,3 +1,9 @@
 export default function ShopItem({item}) {
-    return null;
+    return (
+    <div className="shop-item">
+        <img key={`img-${item.id}`} src={item.imgSrc} alt={item.name} className="shop-item-image"/>
+        <h3 key={`name-${item.id}`} className="shop-item-name">{item.name}</h3>
+        <p key={`price-${item.id}`} className="shop-item-price">{item.price}</p>
+    </div>
+    )
 }
