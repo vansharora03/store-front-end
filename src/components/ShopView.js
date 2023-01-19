4040279747c5f588d93eebdbd50e5f7de7b59763
vansharora03 <vansh1993@icloud.com>
2023-01-19 15:05:27 -0500

    Set up basic shop page structure and components

---
+++ src/components/ShopView.js
@@ -0,0 +1,11 @@
+import ShopItem from "./ShopItem";
+
+export default function ShopView({items}) {
+    return (
+        <div className="ShopView">
+            {items.map(item => {
+                return <ShopItem item={item}/>
+            })}
+        </div>
+    );
+}
