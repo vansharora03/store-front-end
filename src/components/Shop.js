157ab65e56bb35eba9ad04623985f8acc2aa6507
vansharora03 <vansh1993@icloud.com>
2023-01-19 15:06:03 -0500

    Shop page structure and components

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,14 @@
 import NavBar from './NavBar';
+import { useState } from 'react';
+import ShopView from './ShopView';
 
 export default function Shop() {
+    const [items, setItems] = useState([]);
     return (
         <div className="Shop">
             <NavBar/>
             <h1 className="page-title">Shop</h1>
-            
+            <ShopView items={items}/>
         </div>
     );
 }
---
+++ src/components/ShopItem.js
@@ -0,0 +1,3 @@
+export default function ShopItem({item}) {
+    return null;
+}
