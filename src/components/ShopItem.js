8fc391eb632611da4002ad3eb53fc4f0c7e26ab3
vansharora03 <vansh1993@icloud.com>
2023-01-21 18:16:44 -0500

    Fixed onClick for addToCart

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -4,7 +4,7 @@ export default function ShopItem({item, addToCart}) {
         <img key={`img-${item.id}`} src={item.imgSrc} alt={item.name} className="shop-item-image"/>
         <h3 key={`name-${item.id}`} className="shop-item-name">{item.name}</h3>
         <p key={`price-${item.id}`} className="shop-item-price">{item.price}</p>
-        <button key={`add-to-cart-${item.id}`} className="add-to-cart" onClick={addToCart}>Add To Cart</button>
+        <button key={`add-to-cart-${item.id}`} className="add-to-cart" onClick={()=>addToCart(item)}>Add To Cart</button>
     </div>
     )
 }
