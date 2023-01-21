658a8f39d83cdb5b96e0b2ceaa438ace5364616c
vansharora03 <vansh1993@icloud.com>
2023-01-20 23:29:53 -0500

    Added viewable state to Shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,13 +4,16 @@ import data from '../assets/data/shoptItemData';
 
 export default function Shop() {
     const [items, setItems] = useState([]);
+    const [viewable, setViewable] = useState([]);
     useEffect(() => {
         setItems(data);
+        setViewable(data);
     }, [])
+
     return (
         <div className="Shop">
             <h1 className="page-title">Shop</h1>
-            <ShopView items={items}/>
+            <ShopView items={viewable}/>
         </div>
     );
 }
